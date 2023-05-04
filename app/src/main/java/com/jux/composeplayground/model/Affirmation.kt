f10de77ecac8018562d1192a12847894c72d540f
Jux <julien.pedron@gmail.com>
2023-05-04 15:58:51 +0200

    feat : "Affirmations" tutorial
    Complete the "Affirmations" tutorial and build a list with items displaying an image and a piece of text

---
+++ app/src/main/java/com/jux/composeplayground/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.jux.composeplayground.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResId: Int,
+    @DrawableRes val drawableResId: Int
+)
