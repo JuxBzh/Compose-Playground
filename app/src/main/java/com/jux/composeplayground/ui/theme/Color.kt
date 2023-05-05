15697fbca5ebe0af8f94de9e7037c1e5458f52c5
Jux <julien.pedron@gmail.com>
2023-05-05 17:59:32 +0200

    feat : Material Theme & Colors
    Creating a complete set of Material Colors and updating the current theme.

--- app/src/main/java/com/jux/composeplayground/ui/theme/Color.kt
+++ app/src/main/java/com/jux/composeplayground/ui/theme/Color.kt
@@ -2,6 +2,78 @@ package com.jux.composeplayground.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Teal200 = Color(0xFF03DAC5)
+// region Light colors
+
+val light_primary = Color(0xFF006C4C)
+val light_onPrimary = Color(0xFFFFFFFF)
+val light_secondary = Color(0xFF4D6357)
+val light_onSecondary = Color(0xFFFFFFFF)
+val light_tertiary = Color(0xFF3D6373)
+val light_onTertiary = Color(0xFFFFFFFF)
+
+val light_error = Color(0xFFBA1A1A)
+val light_onError = Color(0xFFFFFFFF)
+val light_background = Color(0xFFFFFFFF)
+val light_onBackground = Color(0xFF000000)
+val light_surface = Color(0xFFFBFDF9)
+val light_onSurface = Color(0xFF191C1A)
+val light_surfaceVariant = Color(0xFFDBE5DD)
+val light_onSurfaceVariant = Color(0xFF404943)
+val light_surfaceTint = Color(0xFF006C4C)
+val light_outline = Color(0xFF707973)
+val light_outlineVariant = Color(0xFFBFC9C2)
+
+val light_primaryContainer = Color(0xFF89F8C7)
+val light_onPrimaryContainer = Color(0xFF002114)
+val light_secondaryContainer = Color(0xFFCFE9D9)
+val light_onSecondaryContainer = Color(0xFF092016)
+val light_tertiaryContainer = Color(0xFFC1E8FB)
+val light_onTertiaryContainer = Color(0xFF001F29)
+val light_errorContainer = Color(0xFFFFDAD6)
+val light_onErrorContainer = Color(0xFF410002)
+
+val light_inverseSurface = Color(0xFF2E312F)
+val light_inverseOnSurface = Color(0xFFEFF1ED)
+val light_inversePrimary = Color(0xFF6CDBAC)
+val light_scrim = Color(0xFF000000)
+val light_shadow = Color(0xFF000000)
+
+// endregion
+
+// region Dark colors
+
+val dark_primary = Color(0xFF6CDBAC)
+val dark_onPrimary = Color(0xFF003826)
+val dark_secondary = Color(0xFFB3CCBE)
+val dark_onSecondary = Color(0xFF1F352A)
+val dark_tertiary = Color(0xFFA5CCDF)
+val dark_onTertiary = Color(0xFF073543)
+
+val dark_error = Color(0xFFFFB4AB)
+val dark_onError = Color(0xFF690005)
+val dark_background = Color(0xFF191C1A)
+val dark_onBackground = Color(0xFFE1E3DF)
+val dark_surface = Color(0xFF191c1a)
+val dark_onSurface = Color(0xFFE1E3DF)
+val dark_surfaceVariant = Color(0xFF404943)
+val dark_onSurfaceVariant = Color(0xFFBFC9C2)
+val dark_surfaceTint = Color(0xFF6CDBAC)
+val dark_outline = Color(0xFF8A938C)
+val dark_outlineVariant = Color(0xFF404943)
+
+val dark_primaryContainer = Color(0xFF005138)
+val dark_onPrimaryContainer = Color(0xFF89F8C7)
+val dark_secondaryContainer = Color(0xFF354B40)
+val dark_onSecondaryContainer = Color(0xFFCFE9D9)
+val dark_tertiaryContainer = Color(0xFF244C5B)
+val dark_onTertiaryContainer = Color(0xFFC1E8FB)
+val dark_errorContainer = Color(0xFF93000A)
+val dark_onErrorContainer = Color(0xFFFFDAD6)
+
+val dark_inverseSurface = Color(0xFFE1E3DF)
+val dark_inverseOnSurface = Color(0xFF191C1A)
+val dark_inversePrimary = Color(0xFF006C4C)
+val dark_scrim = Color(0xFF000000)
+val dark_shadow = Color(0xFF000000)
+
+// endregion
--- app/src/main/java/com/jux/composeplayground/ui/theme/Theme.kt
+++ app/src/main/java/com/jux/composeplayground/ui/theme/Theme.kt
@@ -1,43 +1,114 @@
 package com.jux.composeplayground.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorPalette = darkColorScheme(
-    primary = Purple200,
-    secondary = Teal200
+    primary = dark_primary,
+    onPrimary = dark_onPrimary,
+    primaryContainer = dark_primaryContainer,
+    onPrimaryContainer = dark_onPrimaryContainer,
+    secondary = dark_secondary,
+    onSecondary = dark_onSecondary,
+    secondaryContainer = dark_secondaryContainer,
+    onSecondaryContainer = dark_onSecondaryContainer,
+    tertiary = dark_tertiary,
+    onTertiary = dark_onTertiary,
+    tertiaryContainer = dark_tertiaryContainer,
+    onTertiaryContainer = dark_onTertiaryContainer,
+    error = dark_error,
+    onError = dark_onError,
+    errorContainer = dark_errorContainer,
+    onErrorContainer = dark_onErrorContainer,
+    background = dark_background,
+    onBackground = dark_onBackground,
+    surface = dark_surface,
+    onSurface = dark_onSurface,
+    surfaceVariant = dark_surfaceVariant,
+    onSurfaceVariant = dark_onSurfaceVariant,
+    outline = dark_outline,
+    outlineVariant = dark_outlineVariant,
+    surfaceTint = dark_surfaceTint,
+    inverseSurface = dark_inverseSurface,
+    inverseOnSurface = dark_inverseOnSurface,
+    inversePrimary = dark_inversePrimary,
+    scrim = dark_scrim
 )
 
 private val LightColorPalette = lightColorScheme(
-    primary = Purple500,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = light_primary,
+    onPrimary = light_onPrimary,
+    primaryContainer = light_primaryContainer,
+    onPrimaryContainer = light_onPrimaryContainer,
+    secondary = light_secondary,
+    onSecondary = light_onSecondary,
+    secondaryContainer = light_secondaryContainer,
+    onSecondaryContainer = light_onSecondaryContainer,
+    tertiary = light_tertiary,
+    onTertiary = light_onTertiary,
+    tertiaryContainer = light_tertiaryContainer,
+    onTertiaryContainer = light_onTertiaryContainer,
+    error = light_error,
+    onError = light_onError,
+    errorContainer = light_errorContainer,
+    onErrorContainer = light_onErrorContainer,
+    background = light_background,
+    onBackground = light_onBackground,
+    surface = light_surface,
+    onSurface = light_onSurface,
+    surfaceVariant = light_surfaceVariant,
+    onSurfaceVariant = light_onSurfaceVariant,
+    outline = light_outline,
+    outlineVariant = light_outlineVariant,
+    surfaceTint = light_surfaceTint,
+    inverseSurface = light_inverseSurface,
+    inverseOnSurface = light_inverseOnSurface,
+    inversePrimary = light_inversePrimary,
+    scrim = light_scrim
 )
 
 @Composable
 fun ComposePlaygroundTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            when (darkTheme) {
+                true -> dynamicDarkColorScheme(context)
+                else -> dynamicLightColorScheme(context)
+            }
+        }
+
+        darkTheme -> DarkColorPalette
+        else -> LightColorPalette
+    }
+
+    val view = LocalView.current
+    if (view.isInEditMode.not()) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
     }
 
     MaterialTheme(
-        colorScheme = colors,
+        colorScheme = colorScheme,
         typography = Typography,
         shapes = Shapes,
         content = content
